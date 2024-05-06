020ca993de7bdce26411b5ae7a2fcaf9a5e6d5db
sandrine2024 <Sandsol78@gmail.com>
2024-05-06 22:03:08 +0200

    sprint1_ticket3_add: Affichage des pages de la section visiteur

---
+++ src/Controller/Visitor/Blog/BlogController.php
@@ -0,0 +1,18 @@
+<?php
+namespace App\Controller\Visitor\Blog;
+
+
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class WelcomeController extends AbstractController
+{
+    #[Route('/blog', name: 'visitor_blog_index',methods: ['GET'])]
+    public function index(): Response
+    {
+        return $this->render('pages/visitor/blog/index.html.twig');
+    }
+}
---
+++ src/Controller/Visitor/Contact/ContactController.php
@@ -0,0 +1,17 @@
+<?php
+namespace App\Controller\Visitor\Contact;
+
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class ContactController extends AbstractController
+{
+    #[Route('/contact', name: 'visitor_contact_index', methods: ['GET'])]
+    public function index(): Response
+    {
+        return $this->render('pages/visitor/contact/index.html.twig');
+    }
+}
