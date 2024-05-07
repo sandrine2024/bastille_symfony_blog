cc9903802ffcba536c4fb95f8545e60b49703f3c
sandrine2024 <Sandsol78@gmail.com>
2024-05-07 17:02:01 +0200

    sprint2_ticket1_feat: Registration system implemented

--- src/Controller/Visitor/Blog/BlogController.php
+++ src/Controller/Visitor/Blog/BlogController.php
@@ -8,7 +8,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class WelcomeController extends AbstractController
+class BlogController extends AbstractController
 {
     #[Route('/blog', name: 'visitor_blog_index',methods: ['GET'])]
     public function index(): Response
